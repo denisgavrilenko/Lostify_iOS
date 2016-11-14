36576ed8f9cbf965fc389fa50566c4c3d47dc053
Denis Gavrilenko <Denis_Gavrilenko@cedoni.com>
2016-11-14 18:16:42 +0300

    [00010] Add Custom Scroll View

--- Lostify/Lostify/Stories/MapStory/Views/MapViewController.swift
+++ Lostify/Lostify/Stories/MapStory/Views/MapViewController.swift
@@ -8,21 +8,74 @@
 
 import UIKit
 import GoogleMaps
+import SnapKit
 
 class MapViewController: UIViewController {
-    override func loadView() {
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
+    
+    @IBOutlet weak var mapLayoutView: UIView!
+    @IBOutlet weak var cardsView: ReusableScrollView!
+    @IBOutlet weak var movableConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addMapView()
+//        addCardsView()
+        cardsView.dataSource = TestDataSource()
+    }
+    
+    private func addMapView() {
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
-        view = mapView
+        mapView.translatesAutoresizingMaskIntoConstraints = false;
         
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        mapLayoutView.addSubview(mapView)
+        mapView.snp.makeConstraints { (make) in
+            make.edges.equalTo(mapLayoutView).inset(UIEdgeInsetsMake(0, 0, 0, 0))
+        }
     }
 }
+
+struct TestDataSource: ReusableScrollViewDataSource {
+    
+}
+    
+//    private func addCardsView() {
+//        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+//        pageViewController.dataSource = self
+//        
+//        let view = ItemCardViewController(nibName: nil, bundle: nil)
+//        view.responder = self
+//        pageViewController.setViewControllers([view], direction: .forward, animated: false, completion: nil)
+//        
+//        addChildViewController(pageViewController)
+//        cardsView.addSubview(pageViewController.view)
+//        pageViewController.view.snp.makeConstraints { (make) in
+//            make.edges.equalTo(cardsView).inset(UIEdgeInsetsMake(0, 0, 0, 0))
+//        }
+//        pageViewController.didMove(toParentViewController: self)
+//    }
+
+//extension MapViewController: UIPageViewControllerDataSource {
+//    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+//        let view = ItemCardViewController(nibName: nil, bundle: nil)
+//        view.responder = self
+//        return view
+//    }
+//    
+//    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+//        let view = ItemCardViewController(nibName: nil, bundle: nil)
+//        view.responder = self
+//        return view
+//    }
+//}
+//
+//extension MapViewController: PanGestureResponder {
+//    func didPan(viewController: UIViewController) {
+//        UIView.animate(withDuration: 5, delay: 0, options: .curveEaseOut, animations: {
+//            self.movableConstraint.constant = 300
+//            self.view.layoutIfNeeded()
+//        })
+//    }
+//}
