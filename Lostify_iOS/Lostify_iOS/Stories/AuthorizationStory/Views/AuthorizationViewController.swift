3c21d5639976f79e3f1efc867d23b6da9dc48041
Denis Gavrilenko <Denis_Gavrilenko@cedoni.com>
2017-01-14 03:11:25 +0300

    [00021] Add UI for #1 and #2 Sign In/Up screens

--- Lostify_iOS/Lostify_iOS/Stories/AuthorizationStory/Views/AuthorizationViewController.swift
+++ Lostify_iOS/Lostify_iOS/Stories/AuthorizationStory/Views/AuthorizationViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+import Result
 
 class AuthorizationViewController: UIViewController {
     final var router: AuthorizationRouter!
     
+    @IBOutlet var signInButtons: [UIButton]!
+    @IBOutlet weak var signButton: UIButton!
     
-    @IBAction func onLogin(_ sender: AnyObject) {
-        router.navigateToMain(view: self)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signButton.reactive.pressed = CocoaAction(Action<Void, Void, NoError> {
+            SignalProducer { [weak self] observer, _ in
+                self?.animateSignOptions()
+            }
+        })
+    }
+    
+    
+    func onLogin() {
+        
+//        router.navigateToMain(view: self)
+    }
+    
+    // MARK: - Animation
+    
+    // TODO: Move to Behaviour
+    
+    private func animateSignOptions() {
+        UIView.animate(withDuration: 1.5) {
+            self.signButton.alpha = 0;
+            self.signInButtons.forEach{ $0.alpha = 1 }
+        }
+    }
+    
+    // MARK: - Appearance
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
